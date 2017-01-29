f71b1796df3852be8b5bf8a795059e079c49ea3e
Greycrazygamer <dnola@calpoly.edu>
2017-01-28 16:23:23 -0800

    Moved TranformLib into the Libraries Folder

--- engine/src/Libraries/Library.h
+++ engine/src/Libraries/Library.h
@@ -10,11 +10,6 @@ namespace MoonEngine
 		static void Destroy();
 		static std::shared_ptr<MeshLibrary> MeshLib;
 		static std::shared_ptr<ProgramLibrary> ProgramLib;
-<<<<<<< HEAD:engine/src/Loaders/Library.h
 		static std::shared_ptr<TransformLibrary> TransformLib;
-
-
-=======
->>>>>>> origin/master:engine/src/Libraries/Library.h
 	};
 }
