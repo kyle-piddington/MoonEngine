b2a47e56b261fa9f9349fe2565f155aaaa4b0ef2
Kyle Piddington <kpiddy@gmail.com>
2017-01-29 19:30:17 -0800

    Forgot to add hit

---
+++ engine/src/Geometry/Hit.h
@@ -0,0 +1,14 @@
+#pragma once
+/**
+ * Output of a Raycast=
+ */
+namespace MoonEngine
+{
+	struct Hit
+	{
+		GameObject * object;
+		glm::vec3 intersectionPoint;
+		glm::vec3 intersectionNormal;
+		float distance;
+	};	
+}
