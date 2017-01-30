6f9e529da8a79ab1ec3d950ffe7251bca8ec6a5c
hawkfalcon <contact@hawkfalcon.com>
2017-01-29 20:02:45 -0800

    Samplers: switch completely to unordered_map

--- engine/src/GL/GLSampler.cpp
+++ engine/src/GL/GLSampler.cpp
@@ -20,27 +20,27 @@ void GLSampler::init(unordered_map<GLenum, GLint> paramsi, unordered_map<GLenum,
     /* Set defaults if no parameters given */
     if (paramsi.empty() && paramsf.empty()) {
         paramsi = {
-                /* Set wrap modes for the S and T directions */
-                make_tuple(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE),
-                make_tuple(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE),
+            /* Set wrap modes for the S and T directions */
+            {GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE},
+            {GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE},
 
-                /* Set filtering mode for magnification and minification */
-                make_tuple(GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR),
-                make_tuple(GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+            /* Set filtering mode for magnification and minification */
+            {GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR},
+            {GL_TEXTURE_MAG_FILTER, GL_LINEAR}
         };
 
         paramsf = {
-                make_tuple(GL_TEXTURE_MAX_ANISOTROPY_EXT, 16.0f)
+            {GL_TEXTURE_MAX_ANISOTROPY_EXT, 16.0f}
         };
     }
     glGenSamplers(1, &_samplerId);
 
     for (auto &param: paramsi) {
-        glSamplerParameteri(_samplerId, get<0>(param), get<1>(param));
+        glSamplerParameteri(_samplerId, param.first, param.second);
     }
 
     for (auto &param: paramsf) {
-        glSamplerParameterf(_samplerId, get<0>(param), get<1>(param));
+        glSamplerParameterf(_samplerId, param.first, param.second);
     }
 }
 
