5fc0a634fc5b415a13c61c5b7004d58ec53b190f
Arcanis161 <sentinal_zach@live.com>
2017-03-15 22:49:22 -0700

    Wisp in progress -Z

---
+++ engine/src/Component/CharacterComponents/Wisp.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Component/Component.h"
+#include "Component/CollisionComponents/BoxCollider.h"
+#include "Geometry/Transform.h"
+
+namespace MoonEngine
+{
+	class Wisp : public Component
+	{
+	public:
+		Wisp();
+		void start();
+
+		void update(float dt);
+
+		std::shared_ptr<Component> clone() const;
+	private:
+		float accumTime;
+		glm::vec3 direction;
+		std::shared_ptr<GameObject> player;
+	};
+}
