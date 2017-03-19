3d95269ea9b9a0db3f2e0ab398092765b81a5b77
hawkfalcon <contact@hawkfalcon.com>
2017-03-18 17:03:17 -0700

    Tweak the awesome moon animation from @kyle.

--- engine/src/Component/EffectComponents/MoonEffect.cpp
+++ engine/src/Component/EffectComponents/MoonEffect.cpp
@@ -14,10 +14,10 @@ void MoonEffect::start()
 	float x = (float)rand() / RAND_MAX * 6.28f - 3.14;
     float y = (float)rand() / RAND_MAX * 6.28f - 3.14;
 	float z = (float)rand() / RAND_MAX * 6.28f - 3.14;
-	gatherTime = (float)rand()/RAND_MAX + 1.5;
+	gatherTime = (float)rand()/RAND_MAX + 1.4;
 	direction = glm::vec3(x, y, z);
 	player = GetWorld()->getPlayer();
-    scale = glm::vec3(0.15);
+    scale = glm::vec3(0.08);
 	gameObject->getTransform().setScale(scale);
 }
 
@@ -30,8 +30,6 @@ void MoonEffect::update(float dt)
         distance = player->getTransform().getPosition() - gameObject->getTransform().getPosition();
     }
 
-    float x, z;
-    glm::vec3 to;
     gameObject->getTransform().rotate(direction *dt);
     float moveSpeed = -(log(accumTime) - 1) / 3.0f;
 
@@ -47,9 +45,9 @@ void MoonEffect::update(float dt)
 		
      
 		case GATHER:
-			float scale = length(distance) / 4.0f;
-			gameObject->getTransform().translate(normalize(distance) / 2.0f);
-			gameObject->getTransform().setScale(std::min(0.15f, scale));
+			float scale = length(distance) / 16.0f;
+			gameObject->getTransform().translate(normalize(distance));
+			gameObject->getTransform().setScale(std::min(0.08f, scale));
             gameObject->getTransform().rotate(direction *dt);
 
             if (scale <= 0.1f)
