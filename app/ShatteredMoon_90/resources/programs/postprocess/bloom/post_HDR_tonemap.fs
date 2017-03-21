b89123f580905dd5a164afffe5cbc4d3ce61f2c3
hawkfalcon <contact@hawkfalcon.com>
2017-03-21 14:05:17 -0700

    Change to a way better HDR tone mapping model

--- app/ShatteredMoon_90/resources/programs/postprocess/bloom/post_HDR_tonemap.fs
+++ app/ShatteredMoon_90/resources/programs/postprocess/bloom/post_HDR_tonemap.fs
@@ -7,14 +7,25 @@ out vec4 color;
 
 uniform float exposure;
 
+// from https://www.shadertoy.com/view/lslGzl
+vec3 whitePreservingLumaBasedReinhardToneMapping(vec3 color)
+{
+	float white = 2.0;
+	float luma = dot(color, vec3(0.2126, 0.7152, 0.0722));
+	float toneMappedLuma = luma * (1.0 + luma / (white*white)) / (1.0 + luma);
+	color *= toneMappedLuma / luma;
+	//color = pow(color, vec3(1. / gamma));
+	return color;
+}
+
 void main()
 {
-    const float gamma = 2.2;
 
     vec3 hdrColor = texture(hdrTexture, uv).rgb;
 
     // Exposure tone mapping
-    vec3 mapped = vec3(1.0) - exp(-hdrColor * exposure);
+    vec3 mapped = whitePreservingLumaBasedReinhardToneMapping(hdrColor);
+    //mapped = vec3(1.0) - exp(-hdrColor * exposure);
     // Gamma correction
     //mapped = pow(mapped, vec3(1.0 / gamma));
 
